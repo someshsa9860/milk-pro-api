a6c26e75bde9d5f398079b860457b516443183ca
Callvcal <tech.callvcal@gmail.com>
2024-08-21 20:08:06 +0530

    location_id

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
                 'retailers'=>UserData::where('location_id',auth()->user()->location_id)->get(),
                 'staffs'=>User::where('location_id',auth()->user()->location_id)->get(),
                 'orders'=>Order::with('customer')->where('location_id',auth()->user()->location_id)->get(),
-                'rates'=>RateList::where('location_id',auth()->user()->location_id)->get()()
+                'rates'=>RateList::where('location_id',auth()->user()->location_id)->get()
                 
             
             ]
