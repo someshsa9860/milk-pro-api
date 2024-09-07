aa66b36960aac08a4443e8f339d92c9de56f2957
Callvcal <tech.callvcal@gmail.com>
2024-09-07 21:50:05 +0530

    save

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -13,7 +13,7 @@ class Location extends Model
             $location_id=$model->location_id;
             (new RateChart())->check($location_id);
         });
-        static::deleting(function ($model) {
+        static::deleted(function ($model) {
             $location_id=$model->location_id;
             RateList::where('location_id',$location_id)->delete();
         });
