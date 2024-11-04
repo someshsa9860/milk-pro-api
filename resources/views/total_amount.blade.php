a667c9bb4c54462e55480e93089b4fce39c3392b
Callvcal <tech.callvcal@gmail.com>
2024-11-04 12:55:01 +0530

    amt

--- resources/views/total_amount.blade.php
+++ resources/views/total_amount.blade.php
@@ -1,6 +1,4 @@
-<p>
-
-    {{ ($model->cow_amt??0)+($model->buffalo_amt??0)+($model->mixed_amt??0) }}
+{{ ($model->cow_amt??0)+($model->buffalo_amt??0)+($model->mixed_amt??0) }}
     
     
-    </p>
+    
